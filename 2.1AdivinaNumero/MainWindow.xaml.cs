5242487e3e97d9e6aef57adfe7295e139071a125
KrolRyu <mario.martos@iesdoctorbalmis.com>
2021-09-23 12:54:10 +0200

    Agregar archivos de proyecto.

---
+++ 2.1AdivinaNumero/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2._1AdivinaNumero
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random r = new Random();
+        int numero;
+        public MainWindow()
+        {
+            InitializeComponent();
+            numero = r.Next(0, 101);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {            
+            if (int.Parse(Input.Text) == numero)
+            {
+                Resultado.Text = "¡Has acertado!";
+            }
+            else if (int.Parse(Input.Text) > numero)
+            {
+                Resultado.Text = "Te has pasado";
+            }
+            else
+            {
+                Resultado.Text = "Te has quedado corto";
+            }
+        }
+
+        private void Reiniciar_Click(object sender, RoutedEventArgs e)
+        {
+            numero = r.Next(0, 101);
+            Resultado.Text = "";
+            Input.Text = "";
+        }
+    }
+}
